85e7f7d84c1e9aa837509c9fb8fffa77d70c4b5a
Amandaleeanne <A-Schock@outlook.com>
2024-02-07 12:16:04 -0800

    Update uiFunctions.py

--- DNDChoiceAutomater/uiFunctions.py
+++ DNDChoiceAutomater/uiFunctions.py
@@ -3,7 +3,3 @@ from character import Character
 def displayAction():
     """Displays action taken to the messageWindow"""
     pass
-def changeName(name):
-    """Allows user to edit character's Name"""
-    #when button is pressed
-    pass
